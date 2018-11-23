8833aeddd6c5bd6722728f24b3cfc8aa2e41edfa
ltrphuong <ltrphuong@LTRPHUONG.SC.ESILICON.com>
2018-11-23 18:44:32 +0700

    [Topic 04: Dropdown list] In-progress

--- src/selenium_api/Topic_04_DropdownList.java
+++ src/selenium_api/Topic_04_DropdownList.java
@@ -7,9 +7,10 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
@@ -22,23 +23,29 @@ public class Topic_04_DropdownList {
 	WebDriver driver;
 	WebDriverWait waitExplicit;
 
+	JavascriptExecutor javaExecutor;
+
 //	Drop list Job Role 01
 	By xpathDrop1 = By.xpath("//select[@id='job1']");
 
 	@BeforeClass
 	public void beforeClass() {
-		driver = new FirefoxDriver();
-		waitExplicit = new WebDriverWait(driver, 30);
 
-//		System.setProperty("webdriver.chrome.driver", "C:/Phuongcei/workspace/WEBDRIVE_API_07_PHUONGLT/lib/chromedriver.exe");
-//		driver=new ChromeDriver();
+		// driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver",
+				"C:/Phuongcei/workspace/WEBDRIVE_API_07_PHUONGLT/lib/chromedriver.exe");
+		driver = new ChromeDriver();
+
+		// Wait elements are loaded
+		waitExplicit = new WebDriverWait(driver, 30);
 
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 
+		javaExecutor = (JavascriptExecutor) driver;
+
 	}
 
-	@Test
 	public void TC_01_HandleDropDownListHTML() throws InterruptedException {
 //	Step 01 - Truy cập vào trang: https://daominhdam.github.io/basic-form/index.html
 		driver.get("https://daominhdam.github.io/basic-form/index.html");
@@ -70,7 +77,6 @@ public class Topic_04_DropdownList {
 
 	}
 
-	@Test
 	public void TC_02_HandleCustomDropListJQuery() throws InterruptedException {
 //	Step 01 - Truy cập vào trang: http://jqueryui.com/resources/demos/selectmenu/default.html
 		driver.get("http://jqueryui.com/resources/demos/selectmenu/default.html");
@@ -81,58 +87,132 @@ public class Topic_04_DropdownList {
 		// xpath need to point to parent Dropdown list first
 		String xpathElement = "//ul[@id='number-menu']//div[@class='ui-menu-item-wrapper']";
 		String ExpectedItem = "19";
-
-		selectItemInDropdownList(xpathParent, xpathElement, ExpectedItem);
+		String xpathScrollElement = "//label[contains(text(), 'Select a speed')]";
+		
+		selectItemInDropdownList(xpathScrollElement, xpathParent, xpathElement, ExpectedItem);
 
 //	Step 03 - Kiểm tra item đã được chọn thành công
-		driver.findElement(By.xpath("//span[@id='number-button']//span[@class='ui-selectmenu-text' and text()= '" + ExpectedItem + "']")).isDisplayed();
-		
+		Assert.assertTrue(driver.findElement(By.xpath(
+				"//span[@id='number-button']//span[@class='ui-selectmenu-text' and text()= '" + ExpectedItem + "']"))
+				.isDisplayed());
 
+		Thread.sleep(3000);
+	}
+
+	public void TC_03_HandleCustomDropListAngular() throws InterruptedException {
+		/*
+		 * Go to angular examples page scroll to 'Select with reset option/mat-select'
+		 * Click on droplist Scroll to 'Kentucky' and select it Verify selected option
+		 */
+
+		System.out.println("Handle custom dropdown list of Angular");
+		driver.get("https://material.angular.io/components/select/examples");
+
+		String xpathScrollElement = "//div[contains(text(), 'Select with reset option')]";
+		String xpathParent = "//mat-select[@id='mat-select-5']";
+		String xpathElement = "//span[@class='mat-option-text']";
+		String ExpectedItem = "Kentucky";
+
+		selectItemInDropdownList(xpathScrollElement, xpathParent, xpathElement, ExpectedItem);
+
+		Assert.assertTrue(
+				driver.findElement(By.xpath("//span[@class='mat-option-text' and text()='" + ExpectedItem + "']"))
+						.isDisplayed());
+
+		Thread.sleep(3000);
+
+	}
+
+	public void TC_04_HandleCustomDropListTelerik() throws InterruptedException {
+
+		/*
+		 * Go to telerik site Select Orange in CAP COLOR dropbox; verify correct
+		 * selected Select L - 7 1/8" in CAP SIZE; verify correct selected
+		 */
+
+		driver.get("https://demos.telerik.com/kendo-ui/dropdownlist/index");
+
+		String xpathScrollElement ="//div[@id='cap']";
+		String xpathParentColor = "//span[@aria-owns='color_listbox']";
+		String xpathElementColor = "//ul[@id='color_listbox']//li";
+		String ExpectedItemColor = "Orange";
+		selectItemInDropdownList(xpathScrollElement, xpathParentColor, xpathElementColor, ExpectedItemColor);
+		Assert.assertTrue(driver.findElement(By.xpath("//span[@class='k-input' and text()='Orange']")).isDisplayed());
+
+		String xpathParentSize = "//span[@aria-owns='size_listbox']";
+		String xpathElementSize = "//ul[@id='size_listbox']/li";
+		String ExpectedItemSize = "L - 7 1/8\"";
+		selectItemInDropdownList(xpathScrollElement, xpathParentSize, xpathElementSize, ExpectedItemSize);
+		Assert.assertTrue(driver.findElement(By.xpath("//span[@class='k-input' and text()='" + ExpectedItemSize + "']"))
+				.isDisplayed());
+
+		Thread.sleep(3000);
 	}
 
 	@Test
-	public void TC_03_HandleCustomDropListAngular() {
+	public void TC_05_HandleCustomDropListVue() throws InterruptedException {
+		/* Go to Vue dropdown link
+		 * Select Third Option 
+		 * Verify selected 
+		 *  */
 		
+		driver.get("https://mikerodham.github.io/vue-dropdowns/");
+		
+		String xpathScrollElement = "//div[@id='app']/div/li";
+		String xpathParent = "//div[@id='app']/div/li";
+		String xpathElement = "//ul[@class='dropdown-menu']/li";
+		String ExpectedItem = "Third Option";
+		
+		selectItemInDropdownList(xpathScrollElement, xpathParent, xpathElement, ExpectedItem);
+//		Assert.assertTrue(driver.findElement(By.xpath("//div[@id='app']//li[contains(text(), 'Third Option')]")).isDisplayed());
+		
+//		Thread.sleep(7000);
 	}
-	
+
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
 
 	}
 
-	public void selectItemInDropdownList(String xpathParent, String xpathElement, String ExpectedItem) {
-		//===============
+	public void selectItemInDropdownList(String xpathScrollElement, String xpathParent, String xpathElement, String ExpectedItem) {
+		// ===============
 //				xpathParent - là xpath cha = xpath của dropdrown list chứa các options 
 //				xpathElement - là xpath đại diện (general xpath) cho các options trong Element - tương đối 
 //							không phải xpath tuyệt đối đến 1 option cụ thể nào.
 //				ExpectedItem - value của option mình muốn chọn.
-		//===============		
+		// ===============
+
+		// Find the scroll element
+		WebElement element = driver.findElement(By.xpath(xpathScrollElement));
 		
-				// Click on dropdown list
-				WebElement element = driver.findElement(By.xpath(xpathParent));
-				element.click();
+		// Scroll to Scroll element - This is element is used to avoid the case when having a banner cover on
+		javaExecutor.executeScript("arguments[0].scrollIntoView(true);", element);
+		
+		// Click on dropdown list
+		element.click();
 
-				// Create a list to contain all options in dropdown list
-				List<WebElement> optionList = driver.findElements(By.xpath(xpathElement));
+		// Create a list to contain all options in dropdown list
+		List<WebElement> optionList = driver.findElements(By.xpath(xpathElement));
 
-				// Wait for all elements in dropdown list are loaded
-				waitExplicit.until(ExpectedConditions.visibilityOfAllElements(optionList));
+		// Wait for all elements in dropdown list are loaded
+		waitExplicit.until(ExpectedConditions.visibilityOfAllElements(optionList));
 
-				// Go through the list, getText() and compare to expected item
-				// optionList is list contains WebElement => Declare an WebElement: optionElement to go through the list
-				for (WebElement optionElement : optionList) {
-					String textItem = optionElement.getText();
-					System.out.println("Option value: " + textItem);
+		// Go through the list, getText() and compare to expected item
+		// optionList is list contains WebElement => Declare an WebElement:
+		// optionElement to go through the list
+		for (WebElement optionElement : optionList) {
+			String textItem = optionElement.getText();
+			System.out.println("Option value: " + textItem);
 
-					// Check if option value is as expectation. If yes, select that option.
-					if (textItem.equals(ExpectedItem)) {
-						optionElement.click();
-						break;
-					}
-				}
-
+			// Check if option value is as expectation. If yes, select that option.
+			if (textItem.equals(ExpectedItem)) {
+				javaExecutor.executeScript("arguments[0].scrollIntoView(true);", optionElement);
+				optionElement.click();
+				break;
 			}
+		}
 
+	}
 
 }
